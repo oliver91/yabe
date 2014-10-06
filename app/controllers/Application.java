842ef244d97b9e5a7e25339f6f1faa5be41b98af
oliver91 <oliver91@ukr.net>
2014-10-06 14:31:46 +0300

    first commit (creating project)

---
+++ app/controllers/Application.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Application extends Controller
+{
+
+    public static void index()
+    {
+        System.out.println("Yop");
+        render();
+    }
+
+}
