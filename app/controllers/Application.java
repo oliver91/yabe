ce7dabca32f845095ba8d895b31a7d10dbaea050
oliver91 <oliver91@ukr.net>
2014-10-06 20:38:08 +0300

    Viewing and posting comments

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.Post;
 import play.Play;
+import play.data.validation.*;
 import play.mvc.Before;
 import play.mvc.Controller;
 
@@ -28,5 +29,20 @@ public class Application extends Controller
     static void addDefaults() {
         renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
         renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+
+    public static void show(Long id) {
+        Post post = Post.findById(id);
+        render(post);
+    }
+
+    public static void postComment(Long postId, @Required String author, @Required String content) {
+        Post post = Post.findById(postId);
+        if(validation.hasErrors()) {
+            render("Application/show.html", post);
+        }
+        post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+        show(postId);
     }
 }
