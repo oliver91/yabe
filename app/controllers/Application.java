97b5a9b53bbe495183abd0b4dfdd17ad0453ba71
oliver91 <oliver91@ukr.net>
2014-10-06 15:59:07 +0300

    Building the first screen

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,32 @@
 package controllers;
 
+import models.Post;
+import play.Play;
+import play.mvc.Before;
 import play.mvc.Controller;
+
+import java.util.List;
 
 public class Application extends Controller
 {
 
-    public static void index()
-    {
-        System.out.println("Yop");
-        render();
+//    public static void index()
+//    {
+//        System.out.println("Yop");
+//        render();
+//    }
+
+    public static void index() {
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+                "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
     }
 
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
 }
